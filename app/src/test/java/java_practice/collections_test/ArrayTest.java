38c9f48292ee1cb00d60a9f06361839e51d97681
Hiroyuki Sato <hiroysato@gmail.com>
2021-06-30 00:33:47 +0900

    Add List

--- app/src/test/java/java_practice/collections_test/ArrayTest.java
+++ app/src/test/java/java_practice/collections_test/ArrayTest.java
@@ -14,4 +14,28 @@ public class ArrayTest
         List<Integer> myList = new ArrayList<>();
         assertEquals(0,myList.size(),"length");
     }
+
+    @Test
+    void basicListMethodTests(){
+        List fruits = sampleArray();
+
+        assertEquals(4, fruits.size(),"fruits size");
+        fruits.remove(0);
+        assertEquals(3, fruits.size());
+        assertEquals("banana", fruits.get(0), "first fruits name");
+        // List<String> test = List.of("a","b","c");
+
+        fruits.add(0,"grape");
+        assertEquals(4,fruits.size());
+        assertEquals("grape", fruits.get(0));
+    }
+
+    private List sampleArray() {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("peach");
+        fruits.add("orange");
+        return fruits;
+    }
 }
